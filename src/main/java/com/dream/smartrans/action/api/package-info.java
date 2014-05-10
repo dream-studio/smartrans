bbe98621915cdf5bd8e5610dc8df7d2a85a83954
James.Cheung <54218509@qq.com>
2014-05-10 23:36:30 +0800

    新增数据库 PDM

---
+++ src/main/java/com/dream/smartrans/action/api/package-info.java
@@ -0,0 +1,7 @@
+@ParentPackage("convention")  
+@Namespace("/api")
+package com.dream.smartrans.action.api;
+
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+
---
+++ src/main/java/com/dream/smartrans/action/json/package-info.java
@@ -0,0 +1,7 @@
+@ParentPackage("convention")  
+@Namespace("/json")
+package com.dream.smartrans.action.json;
+
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+
